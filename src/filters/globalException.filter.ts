968b88d280152e8f4ac8bff15d7b7f4435f66db9
Vladyslav Supruniuk <Vladyslav.Supruniuk@contracted.pmi.com>
2024-02-18 15:49:16 +0200

    Improvements in global errors catching and errors throwing in auth controller

--- src/filters/globalException.filter.ts
+++ src/filters/globalException.filter.ts
@@ -1,16 +1,12 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common';
-import { HttpArgumentsHost } from '@nestjs/common/interfaces';
-
-import { Response } from 'express';
-
-import { Environments } from '@Enums/Environments.enum';
 import { ResponseStatus } from '@Enums/ResponseStatus.enum';
 import { IValidationError } from '@Interfaces/errors/IValidationError';
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common';
+import { HttpArgumentsHost } from '@nestjs/common/interfaces';
 
 import { ErrorResponseResult } from '@Responses/errorResponses/ErrorResponseResult';
 import { ValidationErrorField } from '@Responses/errors/ValidationErrorField';
 
-import { DateHelper } from '@Helpers/date.helper';
+import { Response } from 'express';
 
 /**
  * Global exception filter for handling all exceptions and errors in app.
@@ -20,41 +16,33 @@ export class GlobalExceptionFilter implements ExceptionFilter {
 	catch(exception: Error, host: ArgumentsHost) {
 		const ctx: HttpArgumentsHost = host.switchToHttp();
 		const response: Response = ctx.getResponse<Response>();
-		const responseResult: ErrorResponseResult<ValidationErrorField> = new ErrorResponseResult(
-			HttpStatus.BAD_REQUEST,
-			ResponseStatus.ERROR,
-		);
 
-		responseResult.title = (exception as unknown as IValidationError).response.error;
+		const responseResult: ErrorResponseResult<ValidationErrorField> =
+			new ErrorResponseResult<ValidationErrorField>(HttpStatus.BAD_REQUEST, ResponseStatus.ERROR);
+
+		responseResult.title = (exception as unknown as IValidationError).response?.error || '';
 		responseResult.code =
 			exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
 
 		responseResult.message =
 			responseResult.code < 500 && responseResult.code >= 400
-				? 'Some fields did not pass validation'
+				? 'Client error'
 				: 'Internal server error';
 
 		responseResult.errors = Array.isArray(
-			(exception as unknown as IValidationError).response.message,
+			(exception as unknown as IValidationError).response?.message || null,
 		)
-			? (exception as unknown as IValidationError).response.message.map((error: string) => {
-					const [message, field] = error.split('|');
+			? (exception as unknown as IValidationError).response?.message?.map((error: string) => {
+					const [message, field = null] = error.split('|');
 
 					return { message, field };
-			  })
+			  }) || []
 			: ([
 					{
-						message: (exception as unknown as IValidationError).response.message,
+						message: (exception as unknown as IValidationError).response?.message || '',
 						field: null,
 					},
 			  ] as unknown as ValidationErrorField[]);
-
-		responseResult.errorsLength = responseResult.errors.length;
-
-		if (process.env.NODE_ENV === Environments.DEV) {
-			responseResult.stack = exception.stack;
-			responseResult.dateTime = DateHelper.dateTimeNow();
-		}
 
 		response.status(responseResult.code).json(responseResult);
 	}
