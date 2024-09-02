1388725e3c2e04ac1aa97e0101f6885f431d1815
Vladyslav Supruniuk <Vladyslav.Supruniuk@contracted.pmi.com>
2024-09-02 23:46:06 +0300

    CTF-117: added query builder mock class

--- src/tests/repositories/directChat/directChats.repository.createChat.spec.ts
+++ src/tests/repositories/directChat/directChats.repository.createChat.spec.ts
@@ -6,93 +6,68 @@ import { NotFoundException } from '@nestjs/common';
 import { DirectChatsRepository } from '@Repositories/directChats.repository';
 import { directChats } from '@TestMocks/DirectChat/directChats';
 import { users } from '@TestMocks/User/users';
-import {
-	DataSource,
-	EntityManager,
-	InsertQueryBuilder,
-	InsertResult,
-	RelationQueryBuilder,
-} from 'typeorm';
+import { DataSource, InsertResult } from 'typeorm';
 import { ObjectLiteral } from 'typeorm/common/ObjectLiteral';
-import { SelectQueryBuilder } from 'typeorm/query-builder/SelectQueryBuilder';
+import { QueryBuilderMock } from '@TestMocks/queryBuilderMock';
 
 describe('directChatRepository', (): void => {
+	let queryBuilderMock: QueryBuilderMock<never>;
+	let dataSourceMock: jest.Mocked<Partial<DataSource>>;
 	let directChatRepository: DirectChatsRepository;
 
 	const executeMockResult: string = 'a9bdc525-1c35-48c0-a0c6-79601d842f43';
+	const directChatsMock: DirectChat[] = [...directChats];
+	const usersMock: User[] = [...users];
 
 	const context: { selectedEntity: 'user' | 'directChat' | ''; whereValue: string } = {
 		selectedEntity: '',
 		whereValue: '',
 	};
-	const directChatsMock: DirectChat[] = [...directChats];
-	const usersMock: User[] = [...users];
-
-	const createQueryBuilderMock: jest.Mock = jest.fn().mockReturnThis();
-	const selectMock: jest.Mock = jest.fn().mockImplementation((entity) => {
-		if (['user', 'directChat'].includes(entity)) {
-			context.selectedEntity = entity;
-		}
-
-		return transactionalEntityManager;
-	});
-	const fromMock: jest.Mock = jest.fn().mockReturnThis();
-	const whereMock: jest.Mock = jest.fn().mockImplementation((_, config: { id: string }) => {
-		context.whereValue = config.id;
-
-		return transactionalEntityManager;
-	});
-	const getOneMock: jest.Mock = jest.fn().mockImplementation(() => {
-		if (context.selectedEntity === 'directChat') {
-			return (
-				directChatsMock.find((directChat: DirectChat) => directChat.id === context.whereValue) ||
-				null
-			);
-		}
-
-		if (context.selectedEntity === 'user') {
-			return usersMock.find((user: User) => user.id === context.whereValue) || null;
-		}
-
-		return null;
-	});
-	const insertMock: jest.Mock = jest.fn().mockReturnThis();
-	const intoMock: jest.Mock = jest.fn().mockReturnThis();
-	const valuesMock: jest.Mock = jest.fn().mockReturnThis();
-	const relationMock: jest.Mock = jest.fn().mockReturnThis();
-	const ofMock: jest.Mock = jest.fn().mockReturnThis();
-	const addMock: jest.Mock = jest.fn().mockReturnThis();
-	const executeMock: jest.Mock = jest.fn().mockImplementation(async (): Promise<InsertResult> => {
-		return <InsertResult>{
-			identifiers: <ObjectLiteral>[{ id: executeMockResult }],
-		};
-	});
-
-	const transactionalEntityManager: Partial<EntityManager> &
-		Partial<SelectQueryBuilder<never>> &
-		Partial<InsertQueryBuilder<never>> &
-		Partial<RelationQueryBuilder<never>> = {
-		createQueryBuilder: createQueryBuilderMock,
-		select: selectMock,
-		from: fromMock,
-		where: whereMock,
-		getOne: getOneMock,
-		insert: insertMock,
-		into: intoMock,
-		values: valuesMock,
-		execute: executeMock,
-		relation: relationMock,
-		of: ofMock,
-		add: addMock,
-	};
-
-	const dataSourceMock: jest.Mocked<Partial<DataSource>> = {
-		transaction: jest.fn().mockImplementation(async (callback) => {
-			await callback(transactionalEntityManager);
-		}),
-	};
 
 	beforeAll((): void => {
+		queryBuilderMock = new QueryBuilderMock<never>();
+
+		queryBuilderMock.select.mockImplementation((entity) => {
+			if (['user', 'directChat'].includes(entity)) {
+				context.selectedEntity = entity;
+			}
+
+			return queryBuilderMock;
+		});
+
+		queryBuilderMock.where.mockImplementation((_, config: { id: string }) => {
+			context.whereValue = config.id;
+
+			return queryBuilderMock;
+		});
+
+		queryBuilderMock.getOne.mockImplementation(() => {
+			if (context.selectedEntity === 'directChat') {
+				return (
+					directChatsMock.find((directChat: DirectChat) => directChat.id === context.whereValue) ||
+					null
+				);
+			}
+
+			if (context.selectedEntity === 'user') {
+				return usersMock.find((user: User) => user.id === context.whereValue) || null;
+			}
+
+			return null;
+		});
+
+		queryBuilderMock.execute.mockImplementation(async (): Promise<InsertResult> => {
+			return <InsertResult>{
+				identifiers: <ObjectLiteral>[{ id: executeMockResult }],
+			};
+		});
+
+		dataSourceMock = {
+			transaction: jest.fn().mockImplementation(async (callback) => {
+				await callback(queryBuilderMock);
+			}),
+		};
+
 		directChatRepository = new DirectChatsRepository(dataSourceMock as DataSource);
 	});
 
@@ -117,56 +92,60 @@ describe('directChatRepository', (): void => {
 		it('should use query builder to create queries for retrieving sender and receiver', async (): Promise<void> => {
 			await directChatRepository.createChat(senderId, receiverId, messageText, messageDateTime);
 
-			expect(createQueryBuilderMock).toHaveBeenCalledTimes(7);
+			expect(queryBuilderMock.createQueryBuilder).toHaveBeenCalledTimes(7);
 
-			expect(selectMock).toHaveBeenCalledTimes(3);
-			expect(selectMock).toHaveBeenNthCalledWith(1, 'user');
-			expect(selectMock).toHaveBeenNthCalledWith(2, 'user');
+			expect(queryBuilderMock.select).toHaveBeenCalledTimes(3);
+			expect(queryBuilderMock.select).toHaveBeenNthCalledWith(1, 'user');
+			expect(queryBuilderMock.select).toHaveBeenNthCalledWith(2, 'user');
 
-			expect(fromMock).toHaveBeenCalledTimes(3);
-			expect(fromMock).toHaveBeenNthCalledWith(1, User, 'user');
-			expect(fromMock).toHaveBeenNthCalledWith(2, User, 'user');
+			expect(queryBuilderMock.from).toHaveBeenCalledTimes(3);
+			expect(queryBuilderMock.from).toHaveBeenNthCalledWith(1, User, 'user');
+			expect(queryBuilderMock.from).toHaveBeenNthCalledWith(2, User, 'user');
 
-			expect(whereMock).toHaveBeenCalledTimes(3);
-			expect(whereMock).toHaveBeenNthCalledWith(1, 'user.id = :id', { id: senderId });
-			expect(whereMock).toHaveBeenNthCalledWith(2, 'user.id = :id', { id: receiverId });
+			expect(queryBuilderMock.where).toHaveBeenCalledTimes(3);
+			expect(queryBuilderMock.where).toHaveBeenNthCalledWith(1, 'user.id = :id', {
+				id: senderId,
+			});
+			expect(queryBuilderMock.where).toHaveBeenNthCalledWith(2, 'user.id = :id', {
+				id: receiverId,
+			});
 
-			expect(getOneMock).toHaveBeenCalledTimes(3);
+			expect(queryBuilderMock.getOne).toHaveBeenCalledTimes(3);
 		});
 
 		it('should use query builder to create query for creating new empty chat', async (): Promise<void> => {
 			await directChatRepository.createChat(senderId, receiverId, messageText, messageDateTime);
 
-			expect(createQueryBuilderMock).toHaveBeenCalledTimes(7);
+			expect(queryBuilderMock.createQueryBuilder).toHaveBeenCalledTimes(7);
 
-			expect(insertMock).toHaveBeenCalledTimes(2);
+			expect(queryBuilderMock.insert).toHaveBeenCalledTimes(2);
 
-			expect(intoMock).toHaveBeenCalledTimes(2);
-			expect(intoMock).toHaveBeenNthCalledWith(1, DirectChat);
+			expect(queryBuilderMock.into).toHaveBeenCalledTimes(2);
+			expect(queryBuilderMock.into).toHaveBeenNthCalledWith(1, DirectChat);
 
-			expect(valuesMock).toHaveBeenCalledTimes(2);
-			expect(valuesMock).toHaveBeenNthCalledWith(1, {});
+			expect(queryBuilderMock.values).toHaveBeenCalledTimes(2);
+			expect(queryBuilderMock.values).toHaveBeenNthCalledWith(1, {});
 
-			expect(executeMock).toHaveBeenCalledTimes(2);
+			expect(queryBuilderMock.execute).toHaveBeenCalledTimes(2);
 		});
 
 		it('should use query builder to create query for retrieving created chat', async (): Promise<void> => {
 			await directChatRepository.createChat(senderId, receiverId, messageText, messageDateTime);
 
-			expect(createQueryBuilderMock).toHaveBeenCalledTimes(7);
+			expect(queryBuilderMock.createQueryBuilder).toHaveBeenCalledTimes(7);
 
-			expect(selectMock).toHaveBeenCalledTimes(3);
-			expect(selectMock).toHaveBeenNthCalledWith(3, 'directChat');
+			expect(queryBuilderMock.select).toHaveBeenCalledTimes(3);
+			expect(queryBuilderMock.select).toHaveBeenNthCalledWith(3, 'directChat');
 
-			expect(fromMock).toHaveBeenCalledTimes(3);
-			expect(fromMock).toHaveBeenNthCalledWith(3, DirectChat, 'directChat');
+			expect(queryBuilderMock.from).toHaveBeenCalledTimes(3);
+			expect(queryBuilderMock.from).toHaveBeenNthCalledWith(3, DirectChat, 'directChat');
 
-			expect(whereMock).toHaveBeenCalledTimes(3);
-			expect(whereMock).toHaveBeenNthCalledWith(3, 'directChat.id = :id', {
+			expect(queryBuilderMock.where).toHaveBeenCalledTimes(3);
+			expect(queryBuilderMock.where).toHaveBeenNthCalledWith(3, 'directChat.id = :id', {
 				id: executeMockResult,
 			});
 
-			expect(getOneMock).toHaveBeenCalledTimes(3);
+			expect(queryBuilderMock.getOne).toHaveBeenCalledTimes(3);
 		});
 
 		it('should use query builder to create query for assigning users to created chat', async (): Promise<void> => {
@@ -179,19 +158,19 @@ describe('directChatRepository', (): void => {
 
 			await directChatRepository.createChat(senderId, receiverId, messageText, messageDateTime);
 
-			expect(createQueryBuilderMock).toHaveBeenCalledTimes(7);
+			expect(queryBuilderMock.createQueryBuilder).toHaveBeenCalledTimes(7);
 
-			expect(relationMock).toHaveBeenCalledTimes(2);
-			expect(relationMock).toHaveBeenNthCalledWith(1, DirectChat, 'users');
-			expect(relationMock).toHaveBeenNthCalledWith(2, DirectChat, 'users');
+			expect(queryBuilderMock.relation).toHaveBeenCalledTimes(2);
+			expect(queryBuilderMock.relation).toHaveBeenNthCalledWith(1, DirectChat, 'users');
+			expect(queryBuilderMock.relation).toHaveBeenNthCalledWith(2, DirectChat, 'users');
 
-			expect(ofMock).toHaveBeenCalledTimes(2);
-			expect(ofMock).toHaveBeenNthCalledWith(1, createdChat);
-			expect(ofMock).toHaveBeenNthCalledWith(2, createdChat);
+			expect(queryBuilderMock.of).toHaveBeenCalledTimes(2);
+			expect(queryBuilderMock.of).toHaveBeenNthCalledWith(1, createdChat);
+			expect(queryBuilderMock.of).toHaveBeenNthCalledWith(2, createdChat);
 
-			expect(addMock).toHaveBeenCalledTimes(2);
-			expect(addMock).toHaveBeenNthCalledWith(1, sender);
-			expect(addMock).toHaveBeenNthCalledWith(2, receiver);
+			expect(queryBuilderMock.add).toHaveBeenCalledTimes(2);
+			expect(queryBuilderMock.add).toHaveBeenNthCalledWith(1, sender);
+			expect(queryBuilderMock.add).toHaveBeenNthCalledWith(2, receiver);
 		});
 
 		it('should use query builder to create query for creating initial chat message', async (): Promise<void> => {
@@ -203,22 +182,22 @@ describe('directChatRepository', (): void => {
 
 			await directChatRepository.createChat(senderId, receiverId, messageText, messageDateTime);
 
-			expect(createQueryBuilderMock).toHaveBeenCalledTimes(7);
+			expect(queryBuilderMock.createQueryBuilder).toHaveBeenCalledTimes(7);
 
-			expect(insertMock).toHaveBeenCalledTimes(2);
+			expect(queryBuilderMock.insert).toHaveBeenCalledTimes(2);
 
-			expect(intoMock).toHaveBeenCalledTimes(2);
-			expect(intoMock).toHaveBeenNthCalledWith(2, DirectChatMessage);
+			expect(queryBuilderMock.into).toHaveBeenCalledTimes(2);
+			expect(queryBuilderMock.into).toHaveBeenNthCalledWith(2, DirectChatMessage);
 
-			expect(valuesMock).toHaveBeenCalledTimes(2);
-			expect(valuesMock).toHaveBeenNthCalledWith(2, {
+			expect(queryBuilderMock.values).toHaveBeenCalledTimes(2);
+			expect(queryBuilderMock.values).toHaveBeenNthCalledWith(2, {
 				dateTime: messageDateTime,
 				messageText: messageText,
 				directChat: createdChat,
 				sender: sender,
 			});
 
-			expect(executeMock).toHaveBeenCalledTimes(2);
+			expect(queryBuilderMock.execute).toHaveBeenCalledTimes(2);
 		});
 
 		it('should use transaction to hande failed queries', async (): Promise<void> => {
--- src/tests/repositories/directChat/directChats.repository.getChats.spec.ts
+++ src/tests/repositories/directChat/directChats.repository.getChats.spec.ts
@@ -3,18 +3,13 @@ import { DirectChat } from '@Entities/DirectChat.entity';
 import { directChats } from '@TestMocks/DirectChat/directChats';
 import { User } from '@Entities/User.entity';
 import { users } from '@TestMocks/User/users';
-import { DataSource, EntityManager } from 'typeorm';
-import { SelectQueryBuilder } from 'typeorm/query-builder/SelectQueryBuilder';
+import { DataSource } from 'typeorm';
 import { DirectChatMessage } from '@Entities/DirectChatMessage.entity';
+import { QueryBuilderMock } from '@TestMocks/queryBuilderMock';
 
 describe('directChatRepository', (): void => {
+	let queryBuilderMock: QueryBuilderMock<never>;
 	let directChatRepository: DirectChatsRepository;
-
-	const context: {
-		selectedEntity: 'directChatMessage.id' | 'DirectChatUsers.directChatId' | 'directChat' | '';
-	} = {
-		selectedEntity: '',
-	};
 
 	const directChatsMock: DirectChat[] = [...directChats];
 	const usersMock: User[] = [...users];
@@ -23,58 +18,38 @@ describe('directChatRepository', (): void => {
 	const lastMessageSubQueryMock: string = 'lastMessageSubQueryMock';
 	const userDirectChatsSubQueryMock: string = 'userDirectChatsSubQueryMock';
 
-	const createQueryBuilderMock: jest.Mock = jest.fn().mockImplementation(() => entityManager);
-	const selectMock: jest.Mock = jest.fn().mockImplementation((entity) => {
-		if (['directChatMessage.id', 'DirectChatUsers.directChatId', 'directChat'].includes(entity)) {
-			context.selectedEntity = entity;
-		}
-
-		return entityManager;
-	});
-	const fromMock: jest.Mock = jest.fn().mockReturnThis();
-	const leftJoinAndSelectMock: jest.Mock = jest.fn().mockReturnThis();
-	const leftJoinAndMapManyMock: jest.Mock = jest.fn().mockReturnThis();
-	const whereMock: jest.Mock = jest.fn().mockReturnThis();
-	const setParameterMock: jest.Mock = jest.fn().mockReturnThis();
-	const orderByMock: jest.Mock = jest.fn().mockReturnThis();
-	const limitMock: jest.Mock = jest.fn().mockReturnThis();
-	const skipMock: jest.Mock = jest.fn().mockReturnThis();
-	const takeMock: jest.Mock = jest.fn().mockReturnThis();
-	const getQueryMock: jest.Mock = jest.fn().mockImplementation(() => {
-		switch (context.selectedEntity) {
-			case 'directChatMessage.id':
-				return lastMessageSubQueryMock;
-
-			case 'DirectChatUsers.directChatId':
-				return userDirectChatsSubQueryMock;
-
-			default:
-				break;
-		}
-	});
-	const getManyMock: jest.Mock = jest.fn().mockImplementation(() => directChatsMock);
-
-	const entityManager: Partial<EntityManager> & Partial<SelectQueryBuilder<never>> = {
-		createQueryBuilder: createQueryBuilderMock,
-		select: selectMock,
-		from: fromMock,
-		leftJoinAndSelect: leftJoinAndSelectMock,
-		leftJoinAndMapMany: leftJoinAndMapManyMock,
-		where: whereMock,
-		setParameter: setParameterMock,
-		orderBy: orderByMock,
-		limit: limitMock,
-		getQuery: getQueryMock,
-		skip: skipMock,
-		take: takeMock,
-		getMany: getManyMock,
-	};
-
-	const dataSourceMock: jest.Mocked<Partial<DataSource>> = {
-		createQueryBuilder: createQueryBuilderMock,
+	const context: {
+		selectedEntity: 'directChatMessage.id' | 'DirectChatUsers.directChatId' | 'directChat' | '';
+	} = {
+		selectedEntity: '',
 	};
 
 	beforeAll((): void => {
+		queryBuilderMock = new QueryBuilderMock();
+
+		queryBuilderMock.select.mockImplementation((entity) => {
+			if (['directChatMessage.id', 'DirectChatUsers.directChatId', 'directChat'].includes(entity)) {
+				context.selectedEntity = entity;
+			}
+
+			return queryBuilderMock;
+		});
+		queryBuilderMock.getQuery.mockImplementation(() => {
+			switch (context.selectedEntity) {
+				case 'directChatMessage.id':
+					return lastMessageSubQueryMock;
+
+				case 'DirectChatUsers.directChatId':
+					return userDirectChatsSubQueryMock;
+
+				default:
+					break;
+			}
+		});
+		queryBuilderMock.getMany.mockImplementation(() => directChatsMock);
+
+		const dataSourceMock: jest.Mocked<Partial<DataSource>> = queryBuilderMock;
+
 		directChatRepository = new DirectChatsRepository(dataSourceMock as DataSource);
 	});
 
@@ -94,44 +69,56 @@ describe('directChatRepository', (): void => {
 		it('should create sub query for retrieving chat last message id', async (): Promise<void> => {
 			await directChatRepository.getLastChats(userIdMock, 1, 1);
 
-			expect(createQueryBuilderMock).toHaveBeenCalledTimes(3);
+			expect(queryBuilderMock.createQueryBuilder).toHaveBeenCalledTimes(3);
 
-			expect(selectMock).toHaveBeenCalledTimes(3);
-			expect(selectMock).toHaveBeenNthCalledWith(1, 'directChatMessage.id');
+			expect(queryBuilderMock.select).toHaveBeenCalledTimes(3);
+			expect(queryBuilderMock.select).toHaveBeenNthCalledWith(1, 'directChatMessage.id');
 
-			expect(fromMock).toHaveBeenCalledTimes(3);
-			expect(fromMock).toHaveBeenNthCalledWith(1, DirectChatMessage, 'directChatMessage');
+			expect(queryBuilderMock.from).toHaveBeenCalledTimes(3);
+			expect(queryBuilderMock.from).toHaveBeenNthCalledWith(
+				1,
+				DirectChatMessage,
+				'directChatMessage',
+			);
 
-			expect(whereMock).toHaveBeenCalledTimes(3);
-			expect(whereMock).toHaveBeenNthCalledWith(
+			expect(queryBuilderMock.where).toHaveBeenCalledTimes(3);
+			expect(queryBuilderMock.where).toHaveBeenNthCalledWith(
 				1,
 				'directChatMessage.directChatId = directChat.id',
 			);
 
-			expect(orderByMock).toHaveBeenCalledTimes(2);
-			expect(orderByMock).toHaveBeenNthCalledWith(1, 'directChatMessage.updatedAt', 'DESC');
+			expect(queryBuilderMock.orderBy).toHaveBeenCalledTimes(2);
+			expect(queryBuilderMock.orderBy).toHaveBeenNthCalledWith(
+				1,
+				'directChatMessage.updatedAt',
+				'DESC',
+			);
 
-			expect(limitMock).toHaveBeenCalledTimes(1);
-			expect(limitMock).toHaveBeenNthCalledWith(1, 1);
+			expect(queryBuilderMock.limit).toHaveBeenCalledTimes(1);
+			expect(queryBuilderMock.limit).toHaveBeenNthCalledWith(1, 1);
 
-			expect(getQueryMock).toHaveBeenCalledTimes(2);
+			expect(queryBuilderMock.getQuery).toHaveBeenCalledTimes(2);
 		});
 
 		it('should create sub query for retrieving user direct chats ids', async (): Promise<void> => {
 			await directChatRepository.getLastChats(userIdMock, 1, 1);
 
-			expect(createQueryBuilderMock).toHaveBeenCalledTimes(3);
+			expect(queryBuilderMock.createQueryBuilder).toHaveBeenCalledTimes(3);
 
-			expect(selectMock).toHaveBeenCalledTimes(3);
-			expect(selectMock).toHaveBeenNthCalledWith(2, 'DirectChatUsers.directChatId');
+			expect(queryBuilderMock.select).toHaveBeenCalledTimes(3);
+			expect(queryBuilderMock.select).toHaveBeenNthCalledWith(2, 'DirectChatUsers.directChatId');
 
-			expect(fromMock).toHaveBeenCalledTimes(3);
-			expect(fromMock).toHaveBeenNthCalledWith(2, 'DirectChatUsers', 'DirectChatUsers');
+			expect(queryBuilderMock.from).toHaveBeenCalledTimes(3);
+			expect(queryBuilderMock.from).toHaveBeenNthCalledWith(
+				2,
+				'DirectChatUsers',
+				'DirectChatUsers',
+			);
 
-			expect(whereMock).toHaveBeenCalledTimes(3);
-			expect(whereMock).toHaveBeenNthCalledWith(2, 'DirectChatUsers.userId = :userId');
+			expect(queryBuilderMock.where).toHaveBeenCalledTimes(3);
+			expect(queryBuilderMock.where).toHaveBeenNthCalledWith(2, 'DirectChatUsers.userId = :userId');
 
-			expect(getQueryMock).toHaveBeenCalledTimes(2);
+			expect(queryBuilderMock.getQuery).toHaveBeenCalledTimes(2);
 		});
 
 		it('should create query for retrieving user direct chats with last message and users info', async (): Promise<void> => {
@@ -140,20 +127,28 @@ describe('directChatRepository', (): void => {
 
 			await directChatRepository.getLastChats(userIdMock, skip, take);
 
-			expect(createQueryBuilderMock).toHaveBeenCalledTimes(3);
+			expect(queryBuilderMock.createQueryBuilder).toHaveBeenCalledTimes(3);
 
-			expect(selectMock).toHaveBeenCalledTimes(3);
-			expect(selectMock).toHaveBeenNthCalledWith(3, 'directChat');
+			expect(queryBuilderMock.select).toHaveBeenCalledTimes(3);
+			expect(queryBuilderMock.select).toHaveBeenNthCalledWith(3, 'directChat');
 
-			expect(fromMock).toHaveBeenCalledTimes(3);
-			expect(fromMock).toHaveBeenNthCalledWith(3, DirectChat, 'directChat');
+			expect(queryBuilderMock.from).toHaveBeenCalledTimes(3);
+			expect(queryBuilderMock.from).toHaveBeenNthCalledWith(3, DirectChat, 'directChat');
 
-			expect(leftJoinAndSelectMock).toHaveBeenCalledTimes(2);
-			expect(leftJoinAndSelectMock).toHaveBeenNthCalledWith(1, 'directChat.users', 'users');
-			expect(leftJoinAndSelectMock).toHaveBeenNthCalledWith(2, 'lastMessage.sender', 'sender');
+			expect(queryBuilderMock.leftJoinAndSelect).toHaveBeenCalledTimes(2);
+			expect(queryBuilderMock.leftJoinAndSelect).toHaveBeenNthCalledWith(
+				1,
+				'directChat.users',
+				'users',
+			);
+			expect(queryBuilderMock.leftJoinAndSelect).toHaveBeenNthCalledWith(
+				2,
+				'lastMessage.sender',
+				'sender',
+			);
 
-			expect(leftJoinAndMapManyMock).toHaveBeenCalledTimes(1);
-			expect(leftJoinAndMapManyMock).toHaveBeenNthCalledWith(
+			expect(queryBuilderMock.leftJoinAndMapMany).toHaveBeenCalledTimes(1);
+			expect(queryBuilderMock.leftJoinAndMapMany).toHaveBeenNthCalledWith(
 				1,
 				'directChat.messages',
 				DirectChatMessage,
@@ -161,25 +156,25 @@ describe('directChatRepository', (): void => {
 				`lastMessage.id = (${lastMessageSubQueryMock})`,
 			);
 
-			expect(whereMock).toHaveBeenCalledTimes(3);
-			expect(whereMock).toHaveBeenNthCalledWith(
+			expect(queryBuilderMock.where).toHaveBeenCalledTimes(3);
+			expect(queryBuilderMock.where).toHaveBeenNthCalledWith(
 				3,
 				`directChat.id IN (${userDirectChatsSubQueryMock})`,
 			);
 
-			expect(setParameterMock).toHaveBeenCalledTimes(1);
-			expect(setParameterMock).toHaveBeenNthCalledWith(1, 'userId', userIdMock);
+			expect(queryBuilderMock.setParameter).toHaveBeenCalledTimes(1);
+			expect(queryBuilderMock.setParameter).toHaveBeenNthCalledWith(1, 'userId', userIdMock);
 
-			expect(orderByMock).toHaveBeenCalledTimes(2);
-			expect(orderByMock).toHaveBeenNthCalledWith(2, 'lastMessage.updatedAt', 'DESC');
+			expect(queryBuilderMock.orderBy).toHaveBeenCalledTimes(2);
+			expect(queryBuilderMock.orderBy).toHaveBeenNthCalledWith(2, 'lastMessage.updatedAt', 'DESC');
 
-			expect(skipMock).toHaveBeenCalledTimes(1);
-			expect(skipMock).toHaveBeenNthCalledWith(1, skip);
+			expect(queryBuilderMock.skip).toHaveBeenCalledTimes(1);
+			expect(queryBuilderMock.skip).toHaveBeenNthCalledWith(1, skip);
 
-			expect(takeMock).toHaveBeenCalledTimes(1);
-			expect(takeMock).toHaveBeenNthCalledWith(1, take);
+			expect(queryBuilderMock.take).toHaveBeenCalledTimes(1);
+			expect(queryBuilderMock.take).toHaveBeenNthCalledWith(1, take);
 
-			expect(getManyMock).toHaveBeenCalledTimes(1);
+			expect(queryBuilderMock.getMany).toHaveBeenCalledTimes(1);
 		});
 
 		it('should return array of Direct Chats', async (): Promise<void> => {
