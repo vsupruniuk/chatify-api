5e117eb8c32f97d8fe6c011ed1d6a9ee3ed7c7b9
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2025-04-13 01:05:35 +0300

    CTF-85: jwt tokens repository reset token by user id method tests update

---
+++ src/tests/unit/repositories/jwtTokens/resetTokenByUserId.spec.ts
@@ -0,0 +1,99 @@
+import { QueryBuilderMock } from '@testMocks/queryBuilderMock';
+import { JwtTokensRepository } from '@repositories/jwtTokens/jwtTokens.repository';
+import { Test, TestingModule } from '@nestjs/testing';
+import { DataSource, UpdateResult } from 'typeorm';
+import { JWTToken } from '@entities/JWTToken.entity';
+import { jwtTokens } from '@testMocks/JWTToken/jwtTokens';
+import { users } from '@testMocks/User/users';
+import { User } from '@entities/User.entity';
+
+describe('JWT token repository', (): void => {
+	const queryBuilderMock: QueryBuilderMock<object> = new QueryBuilderMock<object>();
+
+	let jwtTokensRepository: JwtTokensRepository;
+
+	beforeAll(async (): Promise<void> => {
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			providers: [{ provide: DataSource, useValue: queryBuilderMock }, JwtTokensRepository],
+		}).compile();
+
+		jwtTokensRepository = moduleFixture.get(JwtTokensRepository);
+	});
+
+	describe('Reset token by user id', (): void => {
+		const expectedToken: JWTToken = jwtTokens[0];
+		const expectedUpdateResult: UpdateResult = { raw: [expectedToken], generatedMaps: [] };
+
+		const userIdMock: string = users[5].id;
+		const subqueryMock: string = 'subqueryMock';
+
+		beforeEach((): void => {
+			queryBuilderMock.execute.mockReturnValue(expectedUpdateResult);
+			queryBuilderMock.getQuery.mockReturnValue(subqueryMock);
+		});
+
+		afterEach((): void => {
+			jest.clearAllMocks();
+		});
+
+		it('should be defined', (): void => {
+			expect(jwtTokensRepository.resetTokenByUserId).toBeDefined();
+		});
+
+		it('should be a function', (): void => {
+			expect(jwtTokensRepository.resetTokenByUserId).toBeInstanceOf(Function);
+		});
+
+		it('should use query builder and create a sub query for searching token by user id', async (): Promise<void> => {
+			await jwtTokensRepository.resetTokenByUserId(userIdMock);
+
+			expect(queryBuilderMock.createQueryBuilder).toHaveBeenCalledTimes(2);
+
+			expect(queryBuilderMock.subQuery).toHaveBeenCalledTimes(1);
+
+			expect(queryBuilderMock.select).toHaveBeenCalledTimes(1);
+			expect(queryBuilderMock.select).toHaveBeenNthCalledWith(1, 'jwtToken.id');
+
+			expect(queryBuilderMock.from).toHaveBeenCalledTimes(1);
+			expect(queryBuilderMock.from).toHaveBeenNthCalledWith(1, JWTToken, 'jwtToken');
+
+			expect(queryBuilderMock.innerJoin).toHaveBeenCalledTimes(1);
+			expect(queryBuilderMock.innerJoin).toHaveBeenNthCalledWith(
+				1,
+				User,
+				'user',
+				'user.jwt_token_id = jwtToken.id',
+			);
+
+			expect(queryBuilderMock.where).toHaveBeenCalledTimes(2);
+			expect(queryBuilderMock.where).toHaveBeenNthCalledWith(1, 'user.id = :userId', {
+				userId: userIdMock,
+			});
+
+			expect(queryBuilderMock.getQuery).toHaveBeenCalledTimes(1);
+		});
+
+		it('should use query builder and create a query for updating token to null value', async (): Promise<void> => {
+			await jwtTokensRepository.resetTokenByUserId(userIdMock);
+
+			expect(queryBuilderMock.createQueryBuilder).toHaveBeenCalledTimes(2);
+
+			expect(queryBuilderMock.update).toHaveBeenCalledTimes(1);
+			expect(queryBuilderMock.update).toHaveBeenNthCalledWith(1, JWTToken);
+
+			expect(queryBuilderMock.set).toHaveBeenCalledTimes(1);
+			expect(queryBuilderMock.set).toHaveBeenNthCalledWith(1, { token: null });
+
+			expect(queryBuilderMock.where).toHaveBeenCalledTimes(2);
+			expect(queryBuilderMock.where).toHaveBeenNthCalledWith(2, `id IN (${subqueryMock})`);
+
+			expect(queryBuilderMock.setParameters).toHaveBeenCalledTimes(1);
+			expect(queryBuilderMock.setParameters).toHaveBeenNthCalledWith(1, { userId: userIdMock });
+
+			expect(queryBuilderMock.returning).toHaveBeenCalledTimes(1);
+			expect(queryBuilderMock.returning).toHaveBeenNthCalledWith(1, '*');
+
+			expect(queryBuilderMock.execute).toHaveBeenCalledTimes(1);
+		});
+	});
+});
