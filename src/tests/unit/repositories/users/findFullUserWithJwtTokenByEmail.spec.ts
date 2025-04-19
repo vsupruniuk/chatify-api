953c5eadb5a7b61e1be915b891168d5f5552cef6
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2025-04-19 16:18:06 +0300

    CTF-85: users repository find full user with JWT token by email method tests update

---
+++ src/tests/unit/repositories/users/findFullUserWithJwtTokenByEmail.spec.ts
@@ -0,0 +1,82 @@
+import { QueryBuilderMock } from '@testMocks/queryBuilderMock';
+import { UsersRepository } from '@repositories/users/users.repository';
+import { Test, TestingModule } from '@nestjs/testing';
+import { DataSource } from 'typeorm';
+import { users } from '@testMocks/User/users';
+import { User } from '@entities/User.entity';
+
+describe('Users repository', (): void => {
+	const queryBuilderMock: QueryBuilderMock<object> = new QueryBuilderMock<object>();
+
+	let usersRepository: UsersRepository;
+
+	beforeAll(async (): Promise<void> => {
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			providers: [{ provide: DataSource, useValue: queryBuilderMock }, UsersRepository],
+		}).compile();
+
+		usersRepository = moduleFixture.get(UsersRepository);
+	});
+
+	describe('Find full user with JWT token by email', (): void => {
+		const expectedUser: User = users[3];
+
+		const emailMock: string = expectedUser.email;
+
+		beforeEach((): void => {
+			queryBuilderMock.getOne.mockReturnValue(expectedUser);
+		});
+
+		afterEach((): void => {
+			jest.clearAllMocks();
+		});
+
+		it('should be defined', async (): Promise<void> => {
+			expect(usersRepository.findFullUserWithJwtTokenByEmail).toBeDefined();
+		});
+
+		it('should be a function', async (): Promise<void> => {
+			expect(usersRepository.findFullUserWithJwtTokenByEmail).toBeInstanceOf(Function);
+		});
+
+		it('should use query builder and create a query for searching a user by email with JWT token', async (): Promise<void> => {
+			await usersRepository.findFullUserWithJwtTokenByEmail(emailMock);
+
+			expect(queryBuilderMock.createQueryBuilder).toHaveBeenCalledTimes(1);
+
+			expect(queryBuilderMock.select).toHaveBeenCalledTimes(1);
+			expect(queryBuilderMock.select).toHaveBeenNthCalledWith(1, 'user');
+
+			expect(queryBuilderMock.from).toHaveBeenCalledTimes(1);
+			expect(queryBuilderMock.from).toHaveBeenNthCalledWith(1, User, 'user');
+
+			expect(queryBuilderMock.leftJoinAndSelect).toHaveBeenCalledTimes(1);
+			expect(queryBuilderMock.leftJoinAndSelect).toHaveBeenNthCalledWith(
+				1,
+				'user.jwtToken',
+				'jwtToken',
+			);
+
+			expect(queryBuilderMock.where).toHaveBeenCalledTimes(1);
+			expect(queryBuilderMock.where).toHaveBeenNthCalledWith(1, 'user.email = :email', {
+				email: emailMock,
+			});
+
+			expect(queryBuilderMock.getOne).toHaveBeenCalledTimes(1);
+		});
+
+		it('should return a user if it was found', async (): Promise<void> => {
+			const user: User | null = await usersRepository.findFullUserWithJwtTokenByEmail(emailMock);
+
+			expect(user).toEqual(expectedUser);
+		});
+
+		it('should return null if user was not found', async (): Promise<void> => {
+			queryBuilderMock.getOne.mockReturnValue(null);
+
+			const user: User | null = await usersRepository.findFullUserWithJwtTokenByEmail(emailMock);
+
+			expect(user).toBeNull();
+		});
+	});
+});
