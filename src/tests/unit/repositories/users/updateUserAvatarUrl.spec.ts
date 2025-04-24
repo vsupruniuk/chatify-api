8f318f0a48846316867a5d5f165cc246f3b9a633
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2025-04-24 23:48:35 +0300

    CTF-85: users repository update user avatar url method tests update

---
+++ src/tests/unit/repositories/users/updateUserAvatarUrl.spec.ts
@@ -0,0 +1,62 @@
+import { QueryBuilderMock } from '@testMocks/queryBuilderMock';
+import { UsersRepository } from '@repositories/users/users.repository';
+import { Test, TestingModule } from '@nestjs/testing';
+import { DataSource } from 'typeorm';
+import { users } from '@testMocks/User/users';
+import { User } from '@entities/User.entity';
+
+describe('Users repository', (): void => {
+	const queryBuilderMock: QueryBuilderMock<object> = new QueryBuilderMock<object>();
+
+	let usersRepository: UsersRepository;
+
+	beforeAll(async (): Promise<void> => {
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			providers: [{ provide: DataSource, useValue: queryBuilderMock }, UsersRepository],
+		}).compile();
+
+		usersRepository = moduleFixture.get(UsersRepository);
+	});
+
+	describe('Update user avatar url', (): void => {
+		const userIdMock: string = users[4].id;
+		const userAvatarUrlMock: string | null = users[4].avatarUrl;
+
+		afterEach((): void => {
+			jest.clearAllMocks();
+		});
+
+		it('should be defined', (): void => {
+			expect(usersRepository.updateUserAvatarUrl).toBeDefined();
+		});
+
+		it('should be a function', (): void => {
+			expect(usersRepository.updateUserAvatarUrl).toBeInstanceOf(Function);
+		});
+
+		it('should use query builder and create a query for updating user avatar url', async (): Promise<void> => {
+			await usersRepository.updateUserAvatarUrl(userIdMock, userAvatarUrlMock);
+
+			expect(queryBuilderMock.createQueryBuilder).toHaveBeenCalledTimes(1);
+
+			expect(queryBuilderMock.update).toHaveBeenCalledTimes(1);
+			expect(queryBuilderMock.update).toHaveBeenNthCalledWith(1, User);
+
+			expect(queryBuilderMock.set).toHaveBeenCalledTimes(1);
+			expect(queryBuilderMock.set).toHaveBeenNthCalledWith(1, { avatarUrl: userAvatarUrlMock });
+
+			expect(queryBuilderMock.where).toHaveBeenCalledTimes(1);
+			expect(queryBuilderMock.where).toHaveBeenNthCalledWith(1, 'id = :userId', {
+				userId: userIdMock,
+			});
+
+			expect(queryBuilderMock.execute).toHaveBeenCalledTimes(1);
+		});
+
+		it('should return nothing', async (): Promise<void> => {
+			const result: void = await usersRepository.updateUserAvatarUrl(userIdMock, userAvatarUrlMock);
+
+			expect(result).toBeUndefined();
+		});
+	});
+});
