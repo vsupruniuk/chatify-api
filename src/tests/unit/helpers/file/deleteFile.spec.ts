2d6eac6e789e5304438aedbd69f597068c937eea
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2025-07-16 23:45:53 +0300

    CTF-85: file helper delete file method tests added

---
+++ src/tests/unit/helpers/file/deleteFile.spec.ts
@@ -0,0 +1,40 @@
+import { FileHelper } from '@helpers/file.helper';
+import * as fs from 'fs';
+import * as path from 'path';
+
+describe('File helper', (): void => {
+	describe('delete file', (): void => {
+		const fileName: string = 'user.png';
+
+		beforeEach((): void => {
+			jest.spyOn(fs, 'unlinkSync').mockImplementation(jest.fn());
+			jest.spyOn(path, 'join').mockImplementation((...parts: string[]) => parts.join('/'));
+		});
+
+		afterEach((): void => {
+			jest.restoreAllMocks();
+		});
+
+		it('should be defined', (): void => {
+			expect(FileHelper.deleteFile).toBeDefined();
+		});
+
+		it('should be a function', (): void => {
+			expect(FileHelper.deleteFile).toBeInstanceOf(Function);
+		});
+
+		it('should call join method from path module to build correct path to a folder with public files', (): void => {
+			FileHelper.deleteFile(fileName);
+
+			expect(path.join).toHaveBeenCalledTimes(1);
+			expect(path.join).toHaveBeenNthCalledWith(1, 'public', fileName);
+		});
+
+		it('should call unlink sync method from fs module to delete a file', (): void => {
+			FileHelper.deleteFile(fileName);
+
+			expect(fs.unlinkSync).toHaveBeenCalledTimes(1);
+			expect(fs.unlinkSync).toHaveBeenNthCalledWith(1, `public/${fileName}`);
+		});
+	});
+});
