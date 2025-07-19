01aa19912a7b995b5e16c4f1e4dbccdfdc133122
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2025-07-19 15:55:17 +0300

    CTF-85: response helper set refresh token cookie method tests added

---
+++ src/tests/unit/helpers/reponse/setRefreshTokenCookie.spec.ts
@@ -0,0 +1,42 @@
+import { Response } from 'express';
+import { ResponseHelper } from '@helpers/response.helper';
+import { CookiesNames } from '@enums/CookiesNames.enum';
+
+describe('Response helper', (): void => {
+	const jwtRefreshTokenExpiresInMock: string = '1000';
+
+	beforeAll((): void => {
+		process.env.JWT_REFRESH_TOKEN_EXPIRES_IN = jwtRefreshTokenExpiresInMock;
+	});
+
+	afterAll((): void => {
+		delete process.env.JWT_REFRESH_TOKEN_EXPIRES_IN;
+	});
+
+	describe('Set refresh token', (): void => {
+		const refreshToken: string = 'refreshToken';
+		const response: Response = {
+			cookie: jest.fn(),
+		} as unknown as Response;
+
+		it('should be defined', (): void => {
+			expect(ResponseHelper.setRefreshTokenCookie).toBeDefined();
+		});
+
+		it('should be a function', (): void => {
+			expect(ResponseHelper.setRefreshTokenCookie).toBeInstanceOf(Function);
+		});
+
+		it('should call cookie method in the response object to save refresh token in cookies', (): void => {
+			ResponseHelper.setRefreshTokenCookie(response, refreshToken);
+
+			expect(response.cookie).toHaveBeenCalledTimes(1);
+			expect(response.cookie).toHaveBeenNthCalledWith(1, CookiesNames.REFRESH_TOKEN, refreshToken, {
+				maxAge: Number(jwtRefreshTokenExpiresInMock) * 1000,
+				secure: true,
+				sameSite: 'strict',
+				httpOnly: true,
+			});
+		});
+	});
+});
