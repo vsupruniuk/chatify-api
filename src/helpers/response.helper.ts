bff4b40afeb13e910cbfde3db9157e76c026c8b2
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2025-08-11 18:44:01 +0300

    CTF-85: logout request flow e2e tests added

--- src/helpers/response.helper.ts
+++ src/helpers/response.helper.ts
@@ -1,5 +1,6 @@
 import { Response } from 'express';
 import { CookiesNames } from '@enums/CookiesNames.enum';
+import { Environments } from '@enums/Environments.enum';
 
 /**
  * Helper class for handling response object manipulation
@@ -13,7 +14,7 @@ export class ResponseHelper {
 	public static setRefreshTokenCookie(response: Response, refreshToken: string): void {
 		response.cookie(CookiesNames.REFRESH_TOKEN, refreshToken, {
 			maxAge: Number(process.env.JWT_REFRESH_TOKEN_EXPIRES_IN) * 1000,
-			secure: true,
+			secure: process.env.NODE_ENV !== Environments.TEST,
 			sameSite: 'strict',
 			httpOnly: true,
 		});
