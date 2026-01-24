2af87403281f25e942d9060a337de1741bfff26c
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2026-01-24 12:30:15 +0200

    CTF-125: added test files to coverage and duplication exclusion for sonar qube

--- src/decorators/sanitizing/trim.decorator.ts
+++ src/decorators/sanitizing/trim.decorator.ts
@@ -1,6 +1,12 @@
 import { Transform } from 'class-transformer';
 import { TransformFnParams } from 'class-transformer/types/interfaces';
 
+/**
+ * Trims all whitespaces for a string value and returns it if the values is a string.
+ * Otherwise, returns original value
+ * @constructor TransformFnParams - object with information about filed in DTO where this decorator applied
+ * @returns trimmed value for strings, original value for everything else
+ */
 export const Trim = (): PropertyDecorator => {
 	return Transform((params: TransformFnParams) => {
 		if (typeof params.value === 'string') {
